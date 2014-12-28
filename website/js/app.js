983d2234358364eff1f15b8979a3498c2bd42f17
Rachouan <rachouan_15@hotmail.fr>
2014-12-28 11:06:34 +0100

    update
    update

---
+++ website/js/app.js
@@ -0,0 +1,58 @@
+var mobile = false;
+var mobilewidth = 768;
+var seconds = 0;
+
+$(function(){
+
+	function init () {
+		
+		console.log("INITIALISE");
+
+		$("nav header .menu").on("click",function (e) {
+				e.preventDefault();
+				console.log("click");
+				openMenu();
+			});
+
+
+
+		$(".activities ul li").on("mousedown",function(){
+			$("#header .calendar").addClass("drop");
+		}).on("mouseup",function () {
+			$("#header .calendar").removeClass("drop");
+		});
+		
+	}
+
+	init();
+
+
+	function checkIfMobile (argument) {
+
+		if($(this).width() < mobilewidth){
+
+			$( "html" ).on("swipe",function () {
+				console.log("you swiped");
+			});
+		}else{
+
+		}
+	}
+
+	function openMenu(){
+
+		
+		if($("nav").attr("class") == "open"){
+			$( "nav" ).removeClass("open");
+			$( ".menu" ).removeClass("close");
+			$("article").removeClass("blur");
+			$("#header").removeClass("blur");
+		}else{
+			$( "nav" ).addClass("open");
+			$( ".menu" ).addClass("close");
+			$("article").addClass("blur");
+			$("#header").addClass("blur");
+		}
+	}
+
+})
