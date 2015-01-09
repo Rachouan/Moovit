2efed606abc7d8ade870f71b7fba2e4b0587e8ff
Rachouan <rachouan_15@hotmail.fr>
2015-01-09 14:30:13 +0100

    update

--- website/js/app.js
+++ website/js/app.js
@@ -35,14 +35,11 @@ $(function(){
 	        if(slider.attr("class")== "slider on"){
 	            slider.removeClass('on');
 	            slider.addClass('off');
-	            $(".status").html("private");
-
 	            slider.parent().parent().removeClass("green");
 	            slider.parent().parent().addClass("red");
 	        }else{
 	            slider.removeClass('off');
 	            slider.addClass('on');
-	            $(".status").html("public");
 	            slider.parent().parent().removeClass("red");
 	            slider.parent().parent().addClass("green");
 	        }
